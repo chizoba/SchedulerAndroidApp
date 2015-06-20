18cd1f4c6c1c55dedd2b75c9737dda369892d0d5
chizoba <ogbonnacj2@gmail.com>
2015-06-20 09:03:10 +0100

    initial commit

---
+++ app/src/main/java/com/zobtech/scheduler/IntroActivity.java
@@ -0,0 +1,61 @@
+package com.zobtech.scheduler;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.zobtech.scheduler.slides.FirstSlide;
+import com.zobtech.scheduler.slides.FourthSlide;
+import com.zobtech.scheduler.slides.SecondSlide;
+import com.zobtech.scheduler.slides.ThirdSlide;
+
+
+public class IntroActivity extends AppIntro {
+
+    public SharedPreferences settings;
+    public boolean firstRun;
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        settings = getSharedPreferences("prefs", 0);
+        firstRun = settings.getBoolean("firstRun", true);
+        if (firstRun) {
+            addSlide(new FirstSlide(), getApplicationContext());
+            addSlide(new SecondSlide(), getApplicationContext());
+            addSlide(new ThirdSlide(), getApplicationContext());
+            addSlide(new FourthSlide(), getApplicationContext());
+
+            setFadeAnimation();
+
+        } else{ loadMainActivity();}
+    }
+
+    private void loadMainActivity(){
+
+        settings = getSharedPreferences("prefs", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("firstRun", false);
+        editor.commit();
+        Intent intent = new Intent(this, ScheduleActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onSkipPressed() {
+        loadMainActivity();
+//        Toast.makeText(getApplicationContext(),getString(R.string.skip),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDonePressed() {
+        loadMainActivity();
+    }
+
+    public void getStarted(View v){
+        loadMainActivity();
+    }
+}
+
---
+++ libraries/library/app/src/main/java/com/github/paolorotolo/appintro/AppIntro.java
@@ -0,0 +1,242 @@
+package com.github.paolorotolo.appintro;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public abstract class AppIntro extends FragmentActivity {
+    private PagerAdapter mPagerAdapter;
+    private ViewPager pager;
+    private List<Fragment> fragments = new Vector<Fragment>();
+    private List<ImageView> dots;
+    private int slidesNumber;
+    private Vibrator mVibrator;
+    private boolean isVibrateOn = false;
+    private int vibrateIntensity = 20;
+    private boolean showSkip = true;
+
+    private static int FIRST_PAGE_NUM = 0;
+
+    @Override
+    final protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.intro_layout);
+
+        final TextView skipButton = (TextView) findViewById(R.id.skip);
+        final ImageView nextButton = (ImageView) findViewById(R.id.next);
+        final TextView doneButton = (TextView) findViewById(R.id.done);
+        mVibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isVibrateOn) {
+                    mVibrator.vibrate(vibrateIntensity);
+                }
+                onSkipPressed();
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (isVibrateOn) {
+                    mVibrator.vibrate(vibrateIntensity);
+                }
+                pager.setCurrentItem(pager.getCurrentItem()+1);
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isVibrateOn) {
+                    mVibrator.vibrate(vibrateIntensity);
+                }
+                onDonePressed();
+            }
+        });
+
+        mPagerAdapter = new PagerAdapter(super.getSupportFragmentManager(), fragments);
+        pager = (ViewPager) findViewById(R.id.view_pager);
+        pager.setAdapter(this.mPagerAdapter);
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                selectDot(position);
+                if (position == slidesNumber - 1){
+                    skipButton.setVisibility(View.INVISIBLE);
+                    nextButton.setVisibility(View.GONE);
+                    doneButton.setVisibility(View.VISIBLE);
+                } else {
+                    skipButton.setVisibility(View.VISIBLE);
+                    doneButton.setVisibility(View.GONE);
+                    nextButton.setVisibility(View.VISIBLE);
+                }
+
+                if (!showSkip){
+                    skipButton.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        init(savedInstanceState);
+        loadDots();
+    }
+
+    private void loadDots() {
+        LinearLayout dotLayout = (LinearLayout) findViewById(R.id.dotLayout);
+        dots = new ArrayList<>();
+        slidesNumber = fragments.size();
+
+        for (int i = 0; i < slidesNumber; i++) {
+            ImageView dot = new ImageView(this);
+            dot.setImageDrawable(getResources().getDrawable(R.drawable.indicator_dot_grey));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            dotLayout.addView(dot, params);
+
+            dots.add(dot);
+        }
+
+        selectDot(FIRST_PAGE_NUM);
+    }
+
+    public void selectDot(int index) {
+        Resources res = getResources();
+        for (int i = 0; i < fragments.size(); i++) {
+            int drawableId = (i == index) ? (R.drawable.indicator_dot_white) : (R.drawable.indicator_dot_grey);
+            Drawable drawable = res.getDrawable(drawableId);
+            dots.get(i).setImageDrawable(drawable);
+        }
+    }
+
+    public void addSlide(Fragment fragment, Context context) {
+        fragments.add(Fragment.instantiate(context, fragment.getClass().getName()));
+        mPagerAdapter.notifyDataSetChanged();
+    }
+
+    public List<Fragment> getSlides() {
+        return mPagerAdapter.getFragments();
+    }
+
+    public void setBarColor(final int color){
+        LinearLayout bottomBar = (LinearLayout) findViewById(R.id.bottom);
+        bottomBar.setBackgroundColor(color);
+    }
+
+    public void setSeparatorColor(final int color){
+        TextView separator = (TextView) findViewById(R.id.bottom_separator);
+        separator.setBackgroundColor(color);
+    }
+
+    public void setSkipText(final String text){
+        TextView skipText = (TextView) findViewById(R.id.skip);
+        skipText.setText(text);
+    }
+
+    public void setDoneText(final String text){
+        TextView doneText = (TextView) findViewById(R.id.done);
+        doneText.setText(text);
+    }
+
+    public void showSkipButton(boolean showButton){
+        this.showSkip = showButton;
+        if (!showButton){
+            TextView skip = (TextView) findViewById(R.id.skip);
+            skip.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void setVibrate(boolean vibrate){
+        this.isVibrateOn = vibrate;
+    }
+
+    public void setVibrateIntensity(int intensity){
+        this.vibrateIntensity = intensity;
+    }
+
+    public void setFadeAnimation(){
+        pager.setPageTransformer(true, new FadePageTransformer());
+    }
+
+    public void setCustomTransformer(ViewPager.PageTransformer transformer){
+        pager.setPageTransformer(true, transformer);
+    }
+
+    public abstract void init(Bundle savedInstanceState);
+    public abstract void onSkipPressed();
+    public abstract void onDonePressed();
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments;
+
+        public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return this.fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return this.fragments.size();
+        }
+
+        public List<Fragment> getFragments() {
+            return fragments;
+        }
+
+    }
+
+    public class FadePageTransformer implements ViewPager.PageTransformer {
+        public void transformPage(View view, float position) {
+            view.setTranslationX(view.getWidth() * -position);
+
+            if(position <= -1.0F || position >= 1.0F) {
+                view.setAlpha(0.0F);
+            } else if( position == 0.0F ) {
+                view.setAlpha(1.0F);
+            } else {
+                // position is between -1.0F & 0.0F OR 0.0F & 1.0F
+                view.setAlpha(1.0F - Math.abs(position));
+            }
+        }
+    }
+}
